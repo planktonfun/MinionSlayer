b581c34f9e39701735d1e5606de1279f12c77b16
planktonfun <paulo@esi.ph>
2013-08-25 00:13:50 -0700

    Added Compression between sending and recieving.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var srps  = [];
 var app     = require( 'express' )( )
   , express = require( 'express' )
   , server  = require( 'http' ).createServer( app )
-  , io      = require( 'socket.io' ).listen( server );  
+  , io      = require( 'socket.io' ).listen( server );
 
 
 // connect through http
@@ -82,6 +82,8 @@ io.sockets.on( 'connection', function ( socket ) {
     // message pool for players
     socket.on( 'message', function ( from, msg ) {
 
+        msg.action = lzw_decode( msg.action );
+
         // io.sockets.emit( 'retrieve', { msg: user + ":add:" + user + "\n\n" } );
         SendToAll( user + ":add:" + user + "\n\n" );
 
@@ -140,7 +142,7 @@ io.sockets.on( 'connection', function ( socket ) {
         });
 
         SendToAll( user + ":add:" + user + "\n\n" );
-        
+
         console.log( getUsers( ) );
 
     });
@@ -220,5 +222,59 @@ io.sockets.on( 'connection', function ( socket ) {
     }
 
 
-
 });
+
+
+// LZW-compress a string
+function lzw_encode(s) {
+    var dict = {};
+    var data = (s + "").split("");
+    var out = [];
+    var currChar;
+    var phrase = data[0];
+    var code = 256;
+    for (var i=1; i<data.length; i++) {
+        currChar=data[i];
+        if (dict[phrase + currChar] != null) {
+            phrase += currChar;
+        }
+        else {
+            out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+            dict[phrase + currChar] = code;
+            code++;
+            phrase=currChar;
+        }
+    }
+    out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+    for (var i=0; i<out.length; i++) {
+        out[i] = String.fromCharCode(out[i]);
+    }
+    return out.join("");
+}
+
+
+// Decompress an LZW-encoded string
+function lzw_decode(s) {
+    var dict = {};
+    var data = (s + "").split("");
+    var currChar = data[0];
+    var oldPhrase = currChar;
+    var out = [currChar];
+    var code = 256;
+    var phrase;
+    for (var i=1; i<data.length; i++) {
+        var currCode = data[i].charCodeAt(0);
+        if (currCode < 256) {
+            phrase = data[i];
+        }
+        else {
+           phrase = dict[currCode] ? dict[currCode] : (oldPhrase + currChar);
+        }
+        out.push(phrase);
+        currChar = phrase.charAt(0);
+        dict[code] = oldPhrase + currChar;
+        code++;
+        oldPhrase = phrase;
+    }
+    return out.join("");
+}
